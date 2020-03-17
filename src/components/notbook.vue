a37899b10ff6930ae72bd52092891586251aef5e
yangwj <812406210@qq.com>
2020-03-17 21:35:37 +0800

    请求数据为json

--- src/components/notbook.vue
+++ src/components/notbook.vue
@@ -1,10 +1,49 @@
 <template>
+  <div class="container">
     <h2>新随笔</h2>
+    <h2>练习表格-post请求练习</h2>
+    <table class="table table-bordered">
+      <caption>边框表格布局</caption>
+      <thead>
+      <tr>
+        <th>编号</th>
+        <th>用户名</th>
+        <th>电话</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in list" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.userName}}</td>
+        <td>{{item.phoneNumber}}</td>
+
+      </tr>
+      </tbody>
+    </table>
+  </div>
+
 </template>
 
 <script>
     export default {
-        name: "notbook"
+        name: "notbook",
+      data(){
+        return {
+          list:[]
+        }
+      },
+      created() {
+        //带参请求
+        this.$http.post('http://127.0.0.1:8081/getUserByJson',
+          {
+            "id":"11",
+            "userName":"jie"
+          }//注意，get请求一定要加params，post请求不需要
+        ).then((response) =>{
+          this.list=response.body
+          console.log(this.list)
+        })
+      }
     }
 </script>
 
