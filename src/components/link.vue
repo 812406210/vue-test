cab08ea5a3711e9f3eb12c83e8bcae7e01df048b
yangwj <812406210@qq.com>
2020-03-17 20:31:54 +0800

    后台获取请求数据，展示程列表,post和get请求方式

--- src/components/link.vue
+++ src/components/link.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h2>练习表格</h2>
+    <h2>练习表格-get请求练习</h2>
     <table class="table table-bordered">
       <caption>边框表格布局</caption>
       <thead>
@@ -31,8 +31,11 @@
           }
       },
       created() {
-          this.$http.get('http://127.0.0.1:8081/getUser').then((response) =>{
-            this.list=response.body.user
+          //带参请求
+          this.$http.get('http://127.0.0.1:8081/getUserById',{
+            params:{id:2}//注意，get请求一定要加params，post请求不需要
+                      }).then((response) =>{
+            this.list=response.body
             console.log(this.list)
           })
       }
--- src/components/management.vue
+++ src/components/management.vue
@@ -1,10 +1,47 @@
 <template>
-    <h2>management</h2>
+  <div class="container">
+    <h2>练习表格-post请求练习</h2>
+    <table class="table table-bordered">
+      <caption>边框表格布局</caption>
+      <thead>
+      <tr>
+        <th>编号</th>
+        <th>用户名</th>
+        <th>电话</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in list" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.userName}}</td>
+        <td>{{item.phoneNumber}}</td>
+
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "management"
+        name: "management",
+      data(){
+          return {
+            list:[]
+          }
+      },
+      created() {
+        //带参请求
+        this.$http.post('http://127.0.0.1:8081/getUserByParam',
+          {
+            id:11,
+            userName:'jie'
+          }//注意，get请求一定要加params，post请求不需要
+             ).then((response) =>{
+          this.list=response.body
+          console.log(this.list)
+        })
+      }
     }
 </script>
 
